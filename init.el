c6795735dbfb0fb72fd951c68612b299f6caf5d3
Carl Ola Fornander <ofornander@gmail.com>
2021-11-29 09:17:02 +0100

    Completed parentheses

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("org" . "http://elpa.gnu.org/packages/") t)
 (package-initialize)
 
 (when (not (package-installed-p 'use-package))
