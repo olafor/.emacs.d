d8944df729a2a5ffa61222dddeaccfbb7165a74b
Carl Ola Fornander <ofornander@gmail.com>
2022-02-20 21:49:41 +0100

    quotes?

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (package-initialize)
 
 (when (not (package-installed-p 'use-package))
@@ -21,16 +21,18 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-enabled-themes '(spacemacs-light))
+ '(custom-enabled-themes (quote (spacemacs-light)))
  '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "4eb6fa2ee436e943b168a0cd8eab11afc0752aebb5d974bba2b2ddc8910fca8f" "6bdcff29f32f85a2d99f48377d6bfa362768e86189656f63adbf715ac5c1340b" "78c4238956c3000f977300c8a079a3a8a8d4d9fee2e68bad91123b58a4aa8588" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "83e0376b5df8d6a3fbdfffb9fb0e8cf41a11799d9471293a810deb7586c131e6" default))
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "4eb6fa2ee436e943b168a0cd8eab11afc0752aebb5d974bba2b2ddc8910fca8f" "6bdcff29f32f85a2d99f48377d6bfa362768e86189656f63adbf715ac5c1340b" "78c4238956c3000f977300c8a079a3a8a8d4d9fee2e68bad91123b58a4aa8588" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "83e0376b5df8d6a3fbdfffb9fb0e8cf41a11799d9471293a810deb7586c131e6" default)))
  '(display-time-24hr-format t)
- '(elfeed-goodies/entry-pane-position 'bottom)
+ '(elfeed-goodies/entry-pane-position (quote bottom))
  '(elfeed-goodies/tag-column-width 15)
- '(helm-completion-style 'helm)
+ '(helm-completion-style (quote helm))
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(go-mode restclient-test elfeed-goodies elfeed-dashboard elfeed-org elfeed elixir-mode helm-projectile auto-compile gruvbox-theme spacemacs-theme which-key company helm-bibtex org-appear htmlize org-plus-contrib restclient evil-org doom-modelin projectile rainbow-delimiter evil-surround engine-mode use-package org-bullets org magit helm evil)))
+   (quote
+    (request org-plus-contrib go-mode elfeed-goodies elfeed-dashboard elfeed-org elfeed elixir-mode helm-projectile auto-compile gruvbox-theme spacemacs-theme which-key company helm-bibtex org-appear htmlize evil-org doom-modelin projectile rainbow-delimiter evil-surround engine-mode use-package org-bullets org magit helm evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
